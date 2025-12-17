91df5f453539a943ceb3155f180e2b74e881d22f
Parimal Panda <panda.parimal@gmail.com>
2025-12-17 17:20:52 +0530

    Added Logic for Loop Excersice

---
+++ LoopsExercise.py
@@ -0,0 +1,13 @@
+#Find Second Largest Number in a List
+nums = [12, 35, 1, 10, 34, 40]
+
+first_largest = second_largest = float('-inf')
+
+for num in nums:
+    if num > first_largest:
+        second_largest = first_largest
+        first_largest = num
+    elif num > second_largest and num != first_largest:
+        second_largest = num
+
+print ("The second largest number is:", second_largest)
