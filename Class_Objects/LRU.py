e74f6ce2ef5ce7a544686ed4e6698809eda2e35f
Parimal Panda <panda.parimal@gmail.com>
2025-12-22 11:12:07 +0530

    Added Logic for LRU

---
+++ Class_Objects/LRU.py
@@ -0,0 +1,47 @@
+#LRU Alogrithim Implementation using Dictionary and Doubly Linked List
+class Node:
+    def __init__(self, key, value):
+        self.key = key
+        self.value = value
+        self.prev = None
+        self.next = None
+class LRUCache:
+    def __init__(self, capacity: int):
+        self.capacity = capacity
+        self.cache = {}
+        self.head = Node(0, 0)
+        self.tail = Node(0, 0)
+        self.head.next = self.tail
+        self.tail.prev = self.head
+
+    def _remove(self, node):
+        prev_node = node.prev
+        next_node = node.next
+        prev_node.next = next_node
+        next_node.prev = prev_node
+  #Explain the code for _add_to_head method
+
+    def _add_to_head(self, node):
+        node.prev = self.head
+        node.next = self.head.next
+        self.head.next.prev = node
+        self.head.next = node
+
+    def get(self, key: int) -> int:
+        if key in self.cache:
+            node = self.cache[key]
+            self._remove(node)
+            self._add_to_head(node)
+            return node.value
+        return -1
+
+    def put(self, key: int, value: int) -> None:
+        if key in self.cache:
+            self._remove(self.cache[key])
+        node = Node(key, value)
+        self._add_to_head(node)
+        self.cache[key] = node
+        if len(self.cache) > self.capacity:
+            lru_node = self.tail.prev
+            self._remove(lru_node)
+            del self.cache[lru_node.key]
